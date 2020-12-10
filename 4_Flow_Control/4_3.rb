f878067bf7496ac41492428aa2d02ddba9d5e14d
AK Barratt <andria@akbarratt.com>
2020-12-09 22:58:42 -0500

    Complete exercise 4_3.

---
+++ 4_Flow_Control/4_3.rb
@@ -0,0 +1,14 @@
+puts "Type a number between 0 and 100:"
+number = gets.chomp.to_i
+
+if number < 0
+  puts "Please choose a number between 0 and 100."
+elsif number >= 0 && number <= 50
+  puts"Your number is between 0 and 50."
+elsif number >= 51 && number <= 100
+  puts "Your number is between 51 and 100."
+elsif number > 100
+  puts "Your number is over 100!"
+else
+  puts"Please choose a number between 1 and 100."
+end
