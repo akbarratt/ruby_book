2b2a9ecb593e8a9e35fb6adbd30cc4bb25972c68
AK Barratt <andria@akbarratt.com>
2020-12-14 23:49:03 -0500

    Initialize and begin Chapter 5 exercises.

---
+++ 5_Loops/countdown.rb
@@ -0,0 +1,23 @@
+puts "Enter a number:"
+# x = gets.chomp.to_i
+
+# while x > 0
+#   puts x
+#   x /= 5
+# end
+
+# until x < 0
+#   puts x
+#   x -= 1
+# end
+
+# for i in 1..x do
+#   puts i
+# end
+
+x = [1,2,3,4,5]
+for i in x do
+  puts i
+end
+
+puts "Done!"
